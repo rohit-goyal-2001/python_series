906fc3264c60cf5d427a23c3d41d5d5bff1733ae
rohit-goyal-2001 <cctcctgoyal@gmail.com>
2020-12-07 09:54:56 +0530

    update bool

---
+++ bool.py
@@ -0,0 +1,13 @@
+print(10>9);
+print(12>9);
+print(9<8);
+
+a = 23;
+b = 2.4;
+if b>a:
+    print("b is small");
+else:
+    print("a is big");    
+
+
+print(bool("hello"));    
--- comm.py
+++ comm.py
@@ -7,3 +7,10 @@ print("kiii");
 print("r:\narry");
 print("r:\'narry");
 print("hii \n this is \t rohit");
+#end = "" basically is space chareachter
+#\n use for new line
+#\t use for new tab
+var5 = 12 ;
+txt = "my name is {}"
+print(txt);
+print(txt.format(var5))
